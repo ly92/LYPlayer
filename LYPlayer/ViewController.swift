36e5569f34068c0b11e1d5d15f889d1ccd9651b2
ly92 <1364757394@qq.com>
2017-10-09 14:20:25 +0800

    Initial Commit

---
+++ LYPlayer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LYPlayer
+//
+//  Created by ly on 2017/10/9.
+//  Copyright © 2017年 ly. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
